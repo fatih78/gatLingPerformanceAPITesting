2d2ea9c0936b2be9c885de41eda32f07cb409bcb
fatihtopcuoglu <vmax78@hotmail.com>
2020-09-18 11:15:27 +0200

    pushing first test

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,36 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest
-class DemoApplicationTests {
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.hamcrest.core.StringContains.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class DemoApplicationTests {
+	@LocalServerPort
+	private int port = 8080;
+
+	@Autowired
+	private TestRestTemplate restTemplate;
 
 	@Test
-	void contextLoads() {
+	public void endPointShouldContainRaki1() throws Exception {
+		assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/drinks",
+				String.class)).contains("Raki1");
 	}
+
+//	@Test
+//	public void statusOK() throws Exception {
+//		assertThat(this.restTemplate.getRootUri().
+//	}
 
 }
