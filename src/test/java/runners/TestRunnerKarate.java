1f92b03bcc9e9d44883bac733b9e971aa72512dd
fatihtopcuoglu <vmax78@hotmail.com>
2020-09-29 13:28:29 +0200

    refactor v0.8 with karate framework

--- src/test/java/runners/TestRunnerKarate.java
+++ src/test/java/runners/TestRunnerKarate.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
 @RunWith(Karate.class)
 @CucumberOptions(
         strict = true,
-        features = {"src/test/resources/features/karateGET.feature", "src/test/resources/features/karateCREATE.feature"},
+        features = {"src/test/resources/features/karateGET.feature", "src/test/resources/features/karateCRUD.feature"},
         plugin = {"pretty", "html:target/cucumber-report/jsonfiles", "json:target/cucumber-report/jsonfiles/PatientMS.json"},
         tags = {"@karate"}
         )
