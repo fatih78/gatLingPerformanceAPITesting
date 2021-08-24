6fc73007d297f47829f72168f504abd1857a8bab
Fatih <fatih.topcuoglu@persgroep.net>
2021-08-24 17:06:35 +0200

    refactor

--- src/test/java/runners/TestRunnerKarate.java
+++ src/test/java/runners/TestRunnerKarate.java
@@ -1,16 +1,75 @@
 package runners;
 
 
-import com.intuit.karate.junit4.Karate;
-import cucumber.api.CucumberOptions;
-import org.junit.runner.RunWith;
+import com.intuit.karate.Results;
+import com.intuit.karate.Runner;
+import com.intuit.karate.core.Scenario;
+import com.intuit.karate.core.ScenarioResult;
+import com.intuit.karate.core.Step;
 
-@RunWith(Karate.class)
-@CucumberOptions(
-        strict = true,
-        features = {"src/test/resources/features/karateGET.feature", "src/test/resources/features/karateCRUD.feature"},
-        plugin = {"pretty", "html:target/cucumber-report/jsonfiles", "json:target/cucumber-report/jsonfiles/PatientMS.json"},
-        tags = {"@karate"}
-        )
+import net.masterthought.cucumber.Configuration;
+import net.masterthought.cucumber.ReportBuilder;
+import net.masterthought.cucumber.sorting.SortingMethod;
+import org.apache.commons.io.FileUtils;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.junit.Assert.assertEquals;
+
 public class TestRunnerKarate {
+    public static Results results;
+    static final Logger logger = LoggerFactory.getLogger(runners.TestRunnerKarate.class);
+
+
+    @Test
+    public void testParallel() throws InterruptedException {
+
+        System.setProperty("karate.env", "demo"); // ensure reset if other tests (e.g. mock) had set env in CI
+        results = Runner
+                .path("classpath:features")
+                .outputCucumberJson(true)
+                .tags("~@ignore")
+                .parallel(1);
+        retry();
+
+    }
+
+    //  For now a retry mechanism implemented as workAround. It creates a list of failed scenario's & does 1 rerun on failed scenario's
+    //  Note: The 'Karate' community is busy with a generic solution for this in the upcoming releases.
+    public static void retry() throws InterruptedException {
+        Thread.sleep(1000);
+        if (results.getFailCount() >= 1) {
+            List<ScenarioResult> failed = results.getScenarioResults().filter(sr -> sr.isFailed()).collect(Collectors.toList());
+            for (int i = 0; i < failed.size(); i++) {
+                Scenario scenario = failed.get(i).getScenario();
+                Step step = scenario.getSteps().get(i);
+                results.getSuite().retryScenario(scenario);
+                generateReport(results.getReportDir());
+                logger.error(step.getText());
+                assertEquals(0, results.getFailCount());
+            }
+        } else {
+            generateReport(results.getReportDir());
+            logger.error("Nothing failed, no need to rerun!");
+        }
+    }
+
+    public static void generateReport(String karateOutputPath) {
+        Collection<File> jsonFiles = FileUtils.listFiles(new File(karateOutputPath), new String[]{"json"}, true);
+        List<String> jsonPaths = new ArrayList(jsonFiles.size());
+        jsonFiles.forEach(file -> jsonPaths.add(file.getAbsolutePath()));
+        Configuration config = new Configuration(new File("build"), "KarateAPITest");
+        config.setSortingMethod(SortingMethod.ALPHABETICAL);
+        ReportBuilder reportBuilder = new ReportBuilder(jsonPaths, config);
+        reportBuilder.generateReports();
+    }
 }
+
+
--- src/test/resources/features/karateCRUD.feature
+++ src/test/resources/features/karateCRUD.feature
@@ -2,22 +2,35 @@
 Feature: Testing CRUD: Create, Check and Delete on endpoint
 
   Background:
-    * url 'http://localhost:8000/'
+    * def baseUrl = 'http://localhost:8000/'
     * def fourth = 4
+    * def inputData = read('classpath:inputData/newDrink.json')
+    * def outputData = read('classpath:outputData/newDrinkFanta.json')
 
+  @CreateFantaDrink
   Scenario: creating extra set of data
-    Given path 'drinks'
-    And request {"name":'Fanta', "sort":'non-alcoholic', "abv":0, "id":4, "country":'America'}
+    Given url baseUrl + 'drinks'
+    And request inputData
+    And set inputData.name = "Fanta"
+    And set inputData.sort = "Non-Alcoholic"
+    And set inputData.country = "America"
     When method post
     Then status 201
 
+  @GetFantaDrink
   Scenario: checking extra set of data
-    Given path 'drinks', fourth
+#   The scenario can be runned sstandAlone by calling the right scenario/feature on which it is depending
+    * call read('karateCRUD.feature@CreateFantaDrink')
+    Given url baseUrl + 'drinks/' + 4
     When method get
     Then status 200
-    And match response contains {"name":'Fanta', "sort":'non-alcoholic', "abv":0, "id":4}
+    And match response contains {"name":'Fanta', "sort":'Non-Alcoholic', "abv":0, "id":4}
+#   another check by full json comparison
+    And match response == outputData
 
   Scenario: deleting extra set of data
-    Given path 'drinks', fourth
+    #   The scenario can be runned sstandAlone by calling the right scenario/feature on which it is depending
+    * call read('karateCRUD.feature@GetFantaDrink')
+    Given url baseUrl + 'drinks/' + 4
     When method delete
     Then status 204
